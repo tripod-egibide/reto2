e86770180bcf727a8ded5db797801e5fd1e1e3a6
Aitor <aitorbringas@gmail.com>
2018-11-07 10:04:37 +0100

    Header e index

---
+++ Web/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+    include "./partefija/header.html";
+?>
+
+<?php
+include "./partefija/footer.html";
+?>
+</body>
+</html>

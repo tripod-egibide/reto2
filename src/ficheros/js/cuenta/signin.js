abb6b765a33aac763e7d701573477b3acd879ebb
Sebastián Zawisza <anszau@gmail.com>
2018-11-13 09:43:46 +0100

    actualizacion antes del gran merge

--- src/ficheros/js/cuenta/signin.js
+++ src/ficheros/js/cuenta/signin.js
@@ -1,3 +1,13 @@
-$("#submit").click((event) =>
-  $.post("http://localhost/ficheros/php/cuenta/signin.php", $("#signin").serialize(), (r) => $("#resultado").html(r))
-);
+$("#signin").submit((event) => {
+  let status;
+  $.post(window.root + "/ficheros/php/cuenta/signin.php", $("#signin").serialize(), ((r) => {
+    $("#resultado").html(r)
+    status = r;
+  }));
+  if (status) {
+    header()
+  } else {
+    $(".contra").val("");
+    return false;
+  }
+});

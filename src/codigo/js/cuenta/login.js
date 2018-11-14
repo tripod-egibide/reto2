f6a467b42bff2a4732d2eb9f848b625b6f4a7d26
Sebastián Zawisza <anszau@gmail.com>
2018-11-14 08:56:54 +0100

    progreso general y sql

---
+++ src/codigo/js/cuenta/login.js
@@ -0,0 +1,21 @@
+//al hacer submit
+$("#login").submit(() => {
+  $.ajax({
+    //hacemos un post al php correspondiente, que solo devuelve un mensaje si ha habido un error
+    type: "post",
+    url: window.root + "/codigo/php/cuenta/login.php",
+    data: $("#login").serialize(),
+    success: (r) => {
+      if (r) {
+        //si hay un mensaje, lo mostramos por pantalla y vaciamos los campos de contrasenna
+        $("#resultado").html(r);
+        $(".contra").val("");
+      } else {
+        //si no, vamos al index
+        // TODO: este comportamiento es temporal, en realidad se cerraria el popup
+        window.location.replace(window.root);
+      }
+    }
+  });
+  return false;
+});

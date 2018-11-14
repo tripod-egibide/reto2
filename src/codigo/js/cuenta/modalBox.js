3ef8a6951ece8abe253774ac8a323d2e3653c4e7
Aitor <aitorbringas@gmail.com>
2018-11-14 10:35:45 +0100

    ModalBox

--- src/codigo/js/cuenta/modalBox.js
+++ src/codigo/js/cuenta/modalBox.js
@@ -1,26 +1,18 @@
-var modalLogin = $('#modalLogin');
-var modalSignin = $('#modalSignin');
-var botonLogin  = $('#botonLogin');
-var botonSignin = $('#botonSignin');
-var cerrarSignin = $('#cerrarSignin');
-var cerrarLogin = $('#cerrarLogin');
-
-botonLogin.on("click", function() {
-    modalLogin.show();
+$('#botonLogin').on("click", function() {
+    $('#modalLogin').show();
 });
-botonSignin.on("click", function() {
-    modalSignin.show();
+$('#botonSignin').on("click", function() {
+    $('#modalSignin').show();
 });
 
-cerrarSignin.on("click", function() {
-    modalSignin.hide()
+$('#cerrarSignin').on("click", function() {
+    $('#modalSignin').hide()
 });
-cerrarLogin.on("click", function() {
-    modalLogin.hide();
+$('#cerrarLogin').on("click", function() {
+    $('#modalLogin').hide();
 });
 
-window.on("click", function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
+$('.modal').on("click", function(event) {
+    $('#modalLogin').hide();
+    $('#modalSignin').hide();
 });

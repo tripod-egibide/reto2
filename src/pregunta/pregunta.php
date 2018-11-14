07887de9eb049c4b5c2d901c062857fece3e177c
Sebastián Zawisza <anszau@gmail.com>
2018-11-14 09:53:56 +0100

    estructura de preguntas

---
+++ src/pregunta/pregunta.php
@@ -0,0 +1,40 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>Pregunta</title>
+  </head>
+  <body>
+    <div class="post" id="pregunta">
+      <div class="cabeceraPost" id="cabeceraPregunta">
+        <h1 class="titulo" id="tituloPregunta">Titulo</h1>
+        <span class="informacion" id="informacionPregunta"></span>
+      </div>
+      <div class="votos">
+        <!-- faltarian las flechitas y la variable de los votos -->
+      </div>
+      <p class="cuerpo">Cuerpo</p>
+      <div id="piePregunta">
+        <span id="contadorRespuestas"></span>
+        <ul id="etiquetas">
+          <!-- etiquetas -->
+        </ul>
+      </div>
+    </div>
+
+    <div class="post respuesta">
+      <div class="cabeceraPost">
+        <h2 class="titulo">Titulo</h1>
+        <span class="informacion"></span>
+      </div>
+      <div class="votos">
+        <!-- faltarian las flechitas y la variable de los votos -->
+      </div>
+      <p class="cuerpo">Cuerpo</p>
+    </div>
+  </body>
+</html>

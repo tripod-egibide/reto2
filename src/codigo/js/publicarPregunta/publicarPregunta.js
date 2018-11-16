316bcbe5fd8d96cf3838fe7ef559f130b8a40a24
Unknown <mygoldxp@msn.com>
2018-11-16 12:45:11 +0100

    crear array para las etiquetas varias

--- src/codigo/js/publicarPregunta/publicarPregunta.js
+++ src/codigo/js/publicarPregunta/publicarPregunta.js
@@ -7,5 +7,5 @@ $(document).ready(function (){
                 clearInterval(tiempo);
             }
 
-        }, 10000);
+        }, 2000);
 });
--- src/codigo/php/controller.php
+++ src/codigo/php/controller.php
@@ -27,7 +27,9 @@ session_start();
             header('Location: /error.php?iderror=404');
         }else{
             if($_POST["etiqueta"] != null){
-                if(insertarEtiqueta($_POST["etiqueta"], $idPregunta) == null){
+                //crear array de las etiquetas
+                $etiquetas = dividirEtiquetas();
+                if(!insertarEtiqueta($etiquetas, $idPregunta)){
                     header('Location: /error.php?iderror=404');
                 }else{
                     header('Location: /pregunta/publicarPregunta.php?resultado=publicado');
@@ -35,11 +37,13 @@ session_start();
             }else{
                 header('Location: /pregunta/publicarPregunta.php?resultado=publicado');
             }
-
         }
-
-
-
+    }
+    //divide el string de la etiqueta en varias etiquetas tomando como separador la coma ,
+    function dividirEtiquetas(){
+        $stringFinal = str_replace(' ', ',', $_POST["etiqueta"]);
+        $arrayPalabras = explode(',', $stringFinal);
+        return $arrayPalabras;
     }
     //Pendiente de empezar
     function modificarPregunta(){

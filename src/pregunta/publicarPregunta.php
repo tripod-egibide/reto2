0cb1d192fba0e8a492504aac25bdf2380b895f1d
Unknown <mygoldxp@msn.com>
2018-11-19 08:23:32 +0100

    Actualizar texto front

--- src/pregunta/publicarPregunta.php
+++ src/pregunta/publicarPregunta.php
@@ -5,8 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Tripod</title>
     <link rel="stylesheet" type="text/css" href="../codigo/css/style.css">
-    <script type="text/javascript" src="../codigo/js/jquery-3.3.1.js"></script>
-    <script type="text/javascript" src="../codigo/js/publicarPregunta/publicarPregunta.js"></script>
+    <script type="text/javascript" src="/codigo/js/jquery-3.3.1.js"></script>
+    <script type="text/javascript" src="/codigo/js/publicarPregunta/publicarPregunta.js"></script>
 </head>
 <body>
 <div class="gridContenedor">

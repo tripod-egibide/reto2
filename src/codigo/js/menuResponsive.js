13f8613087c6999ed22fcdf570413923e5fb632c
Sebastián Zawisza <anszau@gmail.com>
2018-11-20 13:25:16 +0100

    pequeñas mejoras de CSS

--- src/codigo/js/menuResponsive.js
+++
@@ -1,9 +0,0 @@
-function interruptorMenu() {
-    if ($("#menuMovil").css("display") === "flex") {
-        $("#menuMovil").css("display", "none");
-    } else {
-        $("#menuMovil").css("display", "flex");
-    }
-    // $("#menuMovil").toggle();
-}
-$("#botonMenu").on("click", interruptorMenu);
